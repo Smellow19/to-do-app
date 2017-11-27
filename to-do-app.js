a85d5687bc950d402a3b4cbb1f1ceed99a48af9b
Tyler Bridges <tylerbridges@Tylers-MacBook-Pro.local>
2017-11-27 11:17:30 -0600

    Up to checkbox checkpoint

--- to-do-app.js
+++ to-do-app.js
@@ -1,5 +1,17 @@
 function onReady() {
-  
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.eventListener('submit', event {
+    event.preventDefault();
+
+    let title = newToDoText.value;
+    let newLi = document.createElement('li');
+    let checkbox = document.createElement('input');
+
+    checkbox.type = 'checkbox';
+  });
 }
 
 window.onload = function () {
