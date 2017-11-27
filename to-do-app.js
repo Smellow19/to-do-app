c1239a0c07a20a84818973bc11faf630e4af491f
Tyler Bridges <tylerbridges@Tylers-MacBook-Pro.local>
2017-11-27 14:03:05 -0600

    Lazy code

--- to-do-app.js
+++ to-do-app.js
@@ -3,17 +3,26 @@ function onReady() {
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.eventListener('submit', event => {
+  addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
     let title = newToDoText.value;
-    let newLi = document.createElement('li');
+    let newLi = document.createElement('li');  // <li> </li>
     let checkbox = document.createElement('input');
+    let button = document.createElement('input')
+
+    button.type = 'button'
+    button.value = 'delete'
+    button.addEventListener('click', function(e){
+      e.target.parentElement.remove()
+    });
 
     checkbox.type = 'checkbox';
-    newli.textContent = title;
+    newLi.textContent = title;
 
     newLi.appendChild(checkbox);
+
+    newLi.appendChild(button);
 
     toDoList.appendChild(newLi);
 
